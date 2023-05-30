a3a11b8906f8d6ee32b2e36f9790990de42607d8
lmigonzalez <lmigonzalezs@gmail.com>
2023-05-30 14:14:27 -0400

    setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,13 @@ export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      my_blue: "#146C94",
+      my_black: "#1A120B",
+      my_green: "#1B9C85",
+      my_red: "#EA5455",
+      my_grey: "#9BA4B5",
+    },
   },
   plugins: [],
 } satisfies Config;
