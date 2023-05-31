c10eb7b21118b037fe0e6439e3b3a84e6baac2d0
joszze <jossecarlos@gmail.com>
2023-05-31 16:43:33 -0400

    setting question req

--- src/axios.ts
+++ src/axios.ts
@@ -1,21 +1,17 @@
+import axios, { type AxiosHeaderValue, type AxiosRequestConfig } from "axios";
 
-import axios, { type AxiosRequestConfig } from "axios";
-
-const config: AxiosRequestConfig = {baseURL: "http://pools-cosmetics.up.railway.app/api"}
+const config: AxiosRequestConfig = {baseURL: "http://pools-cosmetics.up.railway.app/api",}
 const instance = axios.create(config);
 
-export const getCategories = async () => {
-  const res = await instance.get('/get-categories');
+type Question = { id: string };
+
+export const getQuestion = async (quest:Question) => {
+  const res = await instance.get(`/question/${quest.id}`);
+  return res.data as Question;
 };
 
-export const createCategory = () => {
-  return instance.post("/create-category");
-};
-
-export const deleteCategory = () => {
-  return instance.delete("/delete-category");  
-};
-
-export const getProducts = async() => {
-  const res = await instance.get("/get-products");
-};
+export const postQuestion = async (ctx: AxiosHeaderValue, quest: Question) => { 
+  const config: AxiosRequestConfig<Question> = { headers: { ctx: ctx }}
+  const res = await instance.post<Question>("/question", quest, config)
+  return res.status;
+}
