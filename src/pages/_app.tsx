a1ed1b2623b7ab58e5c6686e7eb0bcf1339433a8
joszze <jossecarlos@gmail.com>
2023-05-31 13:34:55 -0400

    restrict acces singin

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,33 @@
-import { ClerkProvider } from "@clerk/nextjs";
+import {
+  ClerkProvider,
+  RedirectToSignIn,
+  SignedIn,
+  SignedOut,
+} from "@clerk/nextjs";
 import { type AppType } from "next/dist/shared/lib/utils";
+import { useRouter } from "next/router";
 import "~/styles/globals.css";
 
+const publicPages = ["/sign-in/[[...index]]", "/sign-up/[[...index]]", "/"];
+
 const MyApp: AppType = ({ Component, pageProps }) => {
+  const { pathname } = useRouter();
+  const isPublicPage = publicPages.includes(pathname);
+
   return (
     <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
+      {isPublicPage ? (
+        <Component {...pageProps} />
+      ) : (
+        <>
+          <SignedIn>
+            <Component {...pageProps} />
+          </SignedIn>
+          <SignedOut>
+            <RedirectToSignIn />
+          </SignedOut>
+        </>
+      )}
     </ClerkProvider>
   );
 };
