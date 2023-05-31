ee4daa7f92972a98a02ae2604c1a4451b590bf1b
lmigonzalez <lmigonzalezs@gmail.com>
2023-05-31 19:30:28 -0400

    add question form

--- src/components/dashboard/AddNewQuestion.tsx
+++ src/components/dashboard/AddNewQuestion.tsx
@@ -1,23 +1,41 @@
 import React, { FC, useState } from "react";
-import { AiOutlinePlus } from "react-icons/ai";
+import {
+  AiOutlinePlus,
+  AiOutlineCloseSquare,
+  AiOutlineMinus,
+} from "react-icons/ai";
 
 interface Field {
-  type: "text" | "file";
+  type: string;
+  value: string | object;
 }
 
 interface QuestionProps {
   closeNewQuestionForm: any;
 }
+
 const AddNewQuestion: React.FC<QuestionProps> = ({ closeNewQuestionForm }) => {
-  const [options, setOptions] = useState(["Option 1", "Option 2"]);
-  const [fields, setFields] = useState([]);
-  const [currentTab, setCurrentTab] = useState(1);
+  const initialData = {
+    question: "",
+    questionImg: {},
+    options: [],
+    answer: "",
+    explanation: [],
+    category: "",
+    complexity: "",
+  };
+  const [currentTab, setCurrentTab] = useState<number>(1);
+  const [newQuestion, setNewQuestion] = useState(initialData);
+  const [options, setOptions] = useState<string[]>(["", ""]);
+  const [questionImage, setQuestionImage] = useState({});
+  const [fields, setFields] = useState<Field[]>([]);
 
   const handleAddOption = () => {
     setOptions([...options, ""]);
   };
 
   const nextTab = () => {
+    console.log(questionImage);
     if (currentTab <= 2) {
       setCurrentTab(currentTab + 1);
     }
@@ -29,21 +47,23 @@ const AddNewQuestion: React.FC<QuestionProps> = ({ closeNewQuestionForm }) => {
     }
   };
 
-  const addTextField = () => {
-    setFields([...fields, { type: "text" }]);
-  };
-
-  const addFileField = () => {
-    setFields([...fields, { type: "file" }]);
+  const handleAddField = (type: string) => {
+    console.log("here");
+    const newField = {
+      type: type,
+      value: "",
+    };
+    setFields([...fields, newField]);
   };
 
   const closeForm = () => {
     closeNewQuestionForm();
   };
 
-  const submitQuestion = () => {
-    console.log("submited");
-    closeNewQuestionForm();
+  const handleDeleteOption = (index: number) => {
+    const updatedOptions = [...options];
+    updatedOptions.splice(index, 1);
+    setOptions(updatedOptions);
   };
 
   const handleOptionChange = (index: number, value: string) => {
@@ -51,8 +71,69 @@ const AddNewQuestion: React.FC<QuestionProps> = ({ closeNewQuestionForm }) => {
     updatedOptions[index] = value;
     setOptions(updatedOptions);
   };
+
+  const handleFieldChange = (index: number, e: any) => {
+    const updatedFields = [...fields];
+    const field = updatedFields[index];
+
+    if (field) {
+      if (e.target && e.target.name === "explanationImage") {
+        console.log(e.target.files[0]);
+        field.value = e.target.files[0];
+      } else {
+        field.value = e.target.value;
+      }
+
+      setFields(updatedFields);
+    }
+  };
+
+  const handleDeleteField = (index: number) => {
+    const updatedFields = [...fields];
+    updatedFields.splice(index, 1);
+    setFields(updatedFields);
+  };
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    setNewQuestion({
+      ...newQuestion,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleQuestionImageFile = (e: any) => {
+    let updatedFile = e.target.files[0];
+    setQuestionImage(updatedFile);
+  };
+
+  const submitQuestion = (e: any) => {
+    e.preventDefault();
+    const updatedQuestion = {
+      ...newQuestion,
+      questionImg: questionImage,
+      options: options,
+      explanation: fields,
+    };
+    console.log(updatedQuestion.questionImg);
+    closeNewQuestionForm();
+    setNewQuestion(initialData);
+    setQuestionImage({});
+    setFields([]);
+  };
+
   return (
-    <div className="fixed left-0 top-0 flex h-screen w-full items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm">
+    <div className="fixed left-0 top-0 flex h-screen w-full flex-col items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm">
+      <div className="flex w-[700px] justify-center">
+        <button
+          onClick={closeForm}
+          className=" transition-all hover:-translate-y-1"
+        >
+          <AiOutlineCloseSquare size={50} fill="#fff" />
+        </button>
+      </div>
+
       <div className="">
         {currentTab === 1 && (
           <div className="flex w-[700px] flex-col bg-white px-4 py-8">
@@ -60,32 +141,79 @@ const AddNewQuestion: React.FC<QuestionProps> = ({ closeNewQuestionForm }) => {
               Add new question and options
             </p>
             <input
+              name="question"
+              value={newQuestion.question}
+              onChange={handleChange}
               type="text"
               placeholder="Question"
               className="my-2 h-10 border-[1px] border-my_black bg-white px-2"
             />
             {options.map((option, index) => (
-              <input
-                className="my-2 h-10 border-[1px] border-my_black bg-white px-2"
+              <div
                 key={index}
-                type="text"
-                placeholder={`Option ${index + 1}`}
-                value={option}
-                onChange={(e) => handleOptionChange(index, e.target.value)}
-              />
+                className="relative my-2 w-full border-[1px] border-my_black bg-red-200"
+              >
+                {" "}
+                <input
+                  className="h-10  w-full bg-white px-2 pr-7"
+                  type="text"
+                  placeholder={`Option ${index + 1}`}
+                  value={option}
+                  onChange={(e) => handleOptionChange(index, e.target.value)}
+                />
+                <button
+                  onClick={() => handleDeleteOption(index)}
+                  className="absolute right-1 top-1/3"
+                >
+                  {" "}
+                  <AiOutlineMinus fill="#EA5455" size={20} />
+                </button>
+              </div>
             ))}
             <div>
-              <button onClick={handleAddOption}>new option</button>
+              <button
+                className="flex items-center gap-1 text-my_blue"
+                onClick={handleAddOption}
+              >
+                new option <AiOutlinePlus />{" "}
+              </button>
             </div>
             <input
+              name="answer"
+              value={newQuestion.answer}
+              onChange={handleChange}
               type="text"
               placeholder="Correct answer"
               className="my-2 h-10 border-[1px] border-my_black bg-white px-2"
             />
+
+            <select
+              name="category"
+              value={newQuestion.category}
+              onChange={handleChange}
+              className="my-2 h-10 border-[1px] border-my_black bg-white px-2"
+            >
+              <option value="">Select Category</option>
+              <option value="practice">Practice</option>
+              <option value="general">General</option>
+              <option value="final">Final</option>
+            </select>
+
+            <select
+              name="complexity"
+              value={newQuestion.complexity}
+              onChange={handleChange}
+              className="my-2 h-10 border-[1px] border-my_black bg-white px-2"
+            >
+              <option value="">Select Complexity</option>
+              <option value="easy">Easy</option>
+              <option value="medium">Medium</option>
+              <option value="hard">Hard</option>
+            </select>
             <div className="mt-8 flex w-full justify-end">
               <button
                 onClick={nextTab}
-                className="bg-my_green px-6 py-1 text-white "
+                className="bg-my_green px-6 py-1 text-white"
               >
                 Next
               </button>
@@ -93,11 +221,20 @@ const AddNewQuestion: React.FC<QuestionProps> = ({ closeNewQuestionForm }) => {
           </div>
         )}
 
+        {/* ----- 2 ----- */}
+
         {currentTab === 2 && (
           <div className="flex w-[700px] flex-col items-center justify-center bg-white px-4 py-8">
-            <p>Add question image</p>
+            <p className="mb-4 text-center text-xl">Add question image</p>
             <div className="mb-4">
-              <input type="file" className="hidden" id="file-input" />
+              <input
+                onChange={handleQuestionImageFile}
+                name="questionImage"
+                type="file"
+                className="hidden"
+                id="file-input"
+                accept="image/*"
+              />
               <label
                 htmlFor="file-input"
                 className="my-6 flex h-40 w-60 cursor-pointer items-center justify-center rounded border border-gray-400 bg-gray-200 px-4 py-2 text-center"
@@ -122,24 +259,46 @@ const AddNewQuestion: React.FC<QuestionProps> = ({ closeNewQuestionForm }) => {
           </div>
         )}
 
+        {/* ----- 3 ------  */}
+
         {currentTab === 3 && (
           <div className="flex w-[700px] flex-col items-center justify-center bg-white px-4 py-8">
-            <div id="fields" className="mb-4 w-full">
+            <p className="mb-4 text-center text-xl">Add question explanation</p>
+
+            <div className="mb-4 w-full">
               {fields.map((field, index) => (
                 <div key={index} className="mb-2 flex justify-start">
                   {field.type === "text" && (
-                    <input
-                      type="text"
-                      placeholder="Enter text"
-                      className="my-2 h-10 w-full border-[1px] border-my_black bg-white px-2"
-                    />
+                    <div
+                      key={index}
+                      className="relative my-2 w-full border-[1px] border-my_black bg-red-200"
+                    >
+                      <input
+                        className="h-10  w-full bg-white px-2 pr-7"
+                        type="text"
+                        placeholder={`Enter Text`}
+                        name="explanationText"
+                        value={field.value}
+                        onChange={(e) => handleFieldChange(index, e)}
+                      />
+                      <button
+                        onClick={() => handleDeleteField(index)}
+                        className="absolute right-1 top-1/3"
+                      >
+                        <AiOutlineMinus fill="#EA5455" size={20} />
+                      </button>
+                    </div>
                   )}
                   {field.type === "file" && (
-                    <>
+                    <div className="flex gap-1">
                       <input
-                        type="file"
+                        name="explanationImage"
                         className="hidden"
                         id={`file-input-${index}`}
+                        type="file"
+                        accept="image/*"
+                        // value={field.value}
+                        onChange={(e) => handleFieldChange(index, e)}
                       />
                       <label
                         htmlFor={`file-input-${index}`}
@@ -147,7 +306,10 @@ const AddNewQuestion: React.FC<QuestionProps> = ({ closeNewQuestionForm }) => {
                       >
                         Select a file
                       </label>
-                    </>
+                      <button onClick={() => handleDeleteField(index)}>
+                        <AiOutlineMinus fill="#EA5455" size={20} />
+                      </button>
+                    </div>
                   )}
                 </div>
               ))}
@@ -155,13 +317,13 @@ const AddNewQuestion: React.FC<QuestionProps> = ({ closeNewQuestionForm }) => {
 
             <div className=" flex w-full justify-start gap-3">
               <button
-                onClick={addFileField}
+                onClick={() => handleAddField("file")}
                 className=" flex items-center justify-center gap-2   px-6 py-1 text-my_blue"
               >
                 Image <AiOutlinePlus />
               </button>
               <button
-                onClick={addTextField}
+                onClick={() => handleAddField("text")}
                 className=" flex items-center justify-center gap-2 px-6 py-1 text-my_blue"
               >
                 Text <AiOutlinePlus />
