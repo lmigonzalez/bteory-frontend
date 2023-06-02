0ffc973664679f0c24b3e67f6c2354c979f57bf7
joszze <jossecarlos@gmail.com>
2023-06-02 01:45:59 -0400

    litle fix

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -6,19 +6,24 @@ import Question from "~/components/Question";
 
 const Test: FC<{ id: string | undefined }> = (props) => {
   const [test, setTest] = useState<Test>({
-    id: "0", questions: [{
-      id: "",
-      question: "",
-      questionImg: "",
-      options: [],
-      answer: "",
-      explanation: [],
-      category: "",
-      complexity: ""
-    }]
+    id: "0",
+    questions: [
+      {
+        id: "",
+        question: "",
+        questionImg: "",
+        options: [],
+        answer: "",
+        explanation: [],
+        category: "",
+        complexity: "",
+      },
+    ],
   });
   const [actQuestion, setActQuestion] = useState<QuestionType>();
-  const [selectedOption, setSelectedOption] = useState<Record<string, Set<number>>>({});
+  const [selectedOption, setSelectedOption] = useState<
+    Record<string, Set<number>>
+  >({});
 
   useEffect(() => {
     if (props.id)
@@ -30,15 +35,13 @@ const Test: FC<{ id: string | undefined }> = (props) => {
   });
 
   function selectOption(select: number) {
-    if (!selectedOption)
-      setSelectedOption((state) => {
-        if (actQuestion) {
-          const set = state[actQuestion.id] ?? new Set<number>();
-          state[actQuestion.id] = set.delete(select) ? set : set.add(select);         
-        }        
-        return state
-      });
-    return;
+    setSelectedOption((state) => {
+      if (actQuestion) {
+        const set = state[actQuestion.id] ?? new Set<number>();
+        state[actQuestion.id] = set.delete(select) ? set : set.add(select);
+      }
+      return state;
+    });
   }
 
   return (
@@ -46,7 +49,11 @@ const Test: FC<{ id: string | undefined }> = (props) => {
       <div className="flex flex-col items-center justify-center ">
         <div className="mt-auto h-8">
           {actQuestion && (
-            <Question question={actQuestion} select={selectOption} selected={selectedOption[actQuestion.id] || new Set<number>()}></Question>
+            <Question
+              question={actQuestion}
+              select={selectOption}
+              selected={selectedOption[actQuestion.id] || new Set<number>()}
+            ></Question>
           )}
           <div className="flex items-center justify-center gap-6">
             <button className="bg-my_black px-4 py-1 text-white">prev</button>
