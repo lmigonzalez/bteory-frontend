e60888c1d60ed9e3c7114dd5a2c4500560fd365b
joszze <jossecarlos@gmail.com>
2023-06-03 03:53:16 -0400

    fixing

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,9 +8,9 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }: any) => {
   return (
-    <div className="relative flex min-h-screen w-full flex-col pt-24  ">
+    <div className="relative flex min-h-screen w-full flex-col pt-16 ">
       <Navbar />
-      <main className="flex w-[1200px] max-w-full flex-1 flex-col  px-4">
+      <main className="flex w-[1200px] max-w-full flex-1 flex-col px-4">
         {children}
       </main>
     </div>
