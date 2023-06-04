6cfc2eb18448b4977ffb8dad420645f52a7d8565
joszze <jossecarlos@gmail.com>
2023-06-04 19:15:09 -0400

    fix

---
+++ src/pages/test/[test].tsx
@@ -0,0 +1,28 @@
+import { AxiosHeaders } from "axios";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
+import { globalState } from "~/Store";
+// pages/category/index.tsx
+const CategoryIndexPage = () => {
+  const {
+    query: { testid },
+  } = useRouter();
+
+  const { test, setTest } = globalState();
+
+  useEffect(() => {
+    void setTest(testid as string, new AxiosHeaders());
+  }, []);
+
+  return (
+    <div>
+      {" "}
+      {
+        // questions
+        test.explanation.toString()
+      }{" "}
+    </div>
+  );
+};
+
+export default CategoryIndexPage;
