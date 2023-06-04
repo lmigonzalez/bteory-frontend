f9126d20a032ffc74b44b79568347aea58db4e83
joszze <jossecarlos@gmail.com>
2023-06-04 17:58:29 -0400

    fixed routes

---
+++ src/pages/test/[test].tsx
@@ -0,0 +1,6 @@
+import React from "react";
+// pages/category/index.tsx
+const CategoryIndexPage = () => {
+  return <div>Category Index </div>;
+};
+export default CategoryIndexPage;
--- src/pages/test/[test].tsx/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const Asd = () => {
-  return <>hola</>;
-};
-export default Asd;
